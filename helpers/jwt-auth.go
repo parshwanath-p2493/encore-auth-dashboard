a624b0f5234cdb2be93e0debfd5f7eca6903cdd4
Parshwanath Paramagond <parshwanathparamagond1234@gmail.com>
2025-04-21 22:55:28 +0530

    files

--- helpers/jwt-auth.go
+++ helpers/jwt-auth.go
@@ -1,7 +1,6 @@
 package helpers
 
 import (
-	"errors"
 	"log"
 	"os"
 	"time"
@@ -60,44 +59,4 @@ func RefreshToken(refreshTokenString string) (string, error) {
 	// Fetch the secret key from .env file
 	SecretKey := os.Getenv("SECRET_KEY")
 
-	// Parse and verify the refresh token
-	refreshToken, err := jwt.Parse(refreshTokenString, func(token *jwt.Token) (interface{}, error) {
-		// Ensure the signing method is HMAC
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New("invalid signing method")
-		}
-		return []byte(SecretKey), nil
-	})
-
-	// Check if the refresh token is valid
-	if err != nil || !refreshToken.Valid {
-		return "", errors.New("invalid refresh token")
-	}
-
-	// Extract claims from the refresh token
-	if claims, ok := refreshToken.Claims.(jwt.MapClaims); ok && refreshToken.Valid {
-		// Extract the name from the claims
-		name, ok := claims["name"].(string)
-		if !ok {
-			return "", errors.New("invalid token claims")
-		}
-
-		// Create a new access token with a 15-minute expiration time
-		newAccessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-			"name": name,
-			"exp":  time.Now().Add(time.Minute * 15).Unix(), // New access token expires in 15 minutes
-		})
-
-		// Sign and generate the new access token string
-		newAccessTokenString, err := newAccessToken.SignedString([]byte(SecretKey))
-		if err != nil {
-			return "", errors.New("failed to generate new access token")
-		}
-
-		// Return the new access token string
-		return newAccessTokenString, nil
-	}
-
-	// If we couldn't extract claims or the token is invalid
-	return "", errors.New("invalid refresh token")
 }
