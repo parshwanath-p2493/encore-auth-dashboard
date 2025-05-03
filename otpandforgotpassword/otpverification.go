f65501b616cc02259b7e5f5322f80d839532c2b3
Parshwanath Paramagond <parshwanathparamagond1234@gmail.com>
2025-05-03 12:22:25 +0530

    commit

--- otpandforgotpassword/otpverification.go
+++ otpandforgotpassword/otpverification.go
@@ -58,3 +58,159 @@ func SendOTP(toName, toEmail string, otp string) error {
 	// Send Email
 	return dialer.DialAndSend(message)
 }
+
+
+
+
+
+
+
+"/guest/signup": {
+      "post": {
+        "tags": ["guest"],
+        "summary": "Guest Signup",
+        "description": "Register a new guest",
+        "parameters": [
+          {
+            "name": "guest",
+            "in": "body",
+            "description": "Guest Signup Details",
+            "required": true,
+            "schema": {
+              "$ref": "#/definitions/Guest"
+            }
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "Guest created successfully",
+            "schema": {
+              "$ref": "#/definitions/GuestResponse"
+            }
+          },
+          "400": {
+            "description": "Invalid request body"
+          },
+          "500": {
+            "description": "Internal server error"
+          }
+        }
+      }
+    },
+    "/guest/login": {
+      "post": {
+        "tags": ["guest"],
+        "summary": "Guest Login",
+        "description": "Authenticate guest user",
+        "parameters": [
+          {
+            "name": "guest",
+            "in": "body",
+            "description": "Guest login credentials",
+            "required": true,
+            "schema": {
+              "$ref": "#/definitions/Guest"
+            }
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "Guest logged in successfully",
+            "schema": {
+              "$ref": "#/definitions/GuestResponse"
+            }
+          },
+          "401": {
+            "description": "Unauthorized - incorrect credentials"
+          },
+          "404": {
+            "description": "Guest not found"
+          },
+          "500": {
+            "description": "Internal server error"
+          }
+        }
+      }
+    },
+    "/guest/logout": {
+      "post": {
+        "tags": ["guest"],
+        "summary": "Guest Logout",
+        "description": "Logs out the guest by invalidating the session token",
+        "responses": {
+          "200": {
+            "description": "Guest logged out successfully",
+            "schema": {
+              "$ref": "#/definitions/GuestLogoutResponse"
+            }
+          },
+          "400": {
+            "description": "Invalid or missing token"
+          },
+          "401": {
+            "description": "Unauthorized"
+          }
+        }
+      }
+    },
+    "/guest/getallguests": {
+      "get": {
+        "tags": ["guest"],
+        "summary": "Get All Guests",
+        "description": "Retrieve all guest users from the database",
+        "responses": {
+          "200": {
+            "description": "List of guests retrieved successfully",
+            "schema": {
+              "type": "array",
+              "items": {
+                "$ref": "#/definitions/Guest"
+              }
+            }
+          },
+          "500": {
+            "description": "Internal server error"
+          }
+        }
+      }
+    }
+  },
+  "definitions": {
+    "Guest": {
+      "type": "object",
+      "required": ["email", "password", "first_name"],
+      "properties": {
+        "first_name": {
+          "type": "string"
+        },
+        "last_name": {
+          "type": "string"
+        },
+        "email": {
+          "type": "string"
+        },
+        "password": {
+          "type": "string"
+        }
+      }
+    },
+    "GuestResponse": {
+      "type": "object",
+      "properties": {
+        "message": {
+          "type": "string"
+        },
+        "data": {
+          "$ref": "#/definitions/Guest"
+        }
+      }
+    },
+    "GuestLogoutResponse": {
+      "type": "object",
+      "properties": {
+        "message": {
+          "type": "string"
+        }
+      }
+    }
+  }
