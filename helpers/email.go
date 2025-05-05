bf2231c737c7ad831edee3b08550c0382f297b79
Parshwanath Paramagond <parshwanathparamagond1234@gmail.com>
2025-05-05 23:30:10 +0530

    commits

--- helpers/email.go
+++ helpers/email.go
@@ -79,3 +79,177 @@ func SendMail(toName, toEmail string) error {
 	// Send Email
 	return dialer.DialAndSend(message)
 }
+
+
+
+
+package helpers
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/gomail.v2"
+)
+
+/*
+**
+
+var (
+
+	fromName  string
+	fromEmail string
+	key       string
+
+)
+
+	func init() {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+		fromName = os.Getenv("FROM_NAME")
+		fromEmail = os.Getenv("FROM_EMAIL")
+		key = os.Getenv("SENDGRID_API_KEY")
+
+}
+
+	func SendMail(toName, toEmail string) (string, error) {
+		from := mail.NewEmail(fromName, fromEmail)
+		to := mail.NewEmail(toName, toEmail)
+		subject := "Sending with Twilio SendGrid is Fun"
+		plainTextContent := "and easy to do anywhere, even with Go"
+		htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+
+		message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+
+		client := sendgrid.NewSendClient(key)
+		response, err := client.Send(message)
+		if err != nil {
+			log.Println("SendGrid error:", err)
+			return "", err
+		}
+
+		fmt.Println("Status:", response.StatusCode)
+		fmt.Println("Body:", response.Body)
+		fmt.Println("Headers:", response.Headers)
+
+		return "Welcome message sent successfully", nil
+	}
+
+*
+*/
+var plainText = "and easy to do anywhere, even with Go"
+var htmlContent = fmt.Sprint("<strong> 🔔 HI WELCOME TO OUR APP</strong>")
+
+var subject = "WEKCOME MESSAGE "
+
+func SendMail(toName, toEmail string) error {
+	fromEmail := "parshwanathparamagond1234@gmail.com"
+	//fromPassword := "fbfy zhlt csqr djay"
+	fromPassword := os.Getenv("KEY")
+
+	smtpHost := "smtp.gmail.com"
+	smtpPort := 587
+	message := gomail.NewMessage()
+	message.SetHeader("From", fromEmail)
+	message.SetHeader("To", toEmail)
+	message.SetHeader("Subject", subject)
+	message.SetBody("text/plain", plainText)
+	message.AddAlternative("text/html", htmlContent)
+
+	dialer := gomail.NewDialer(smtpHost, smtpPort, fromEmail, fromPassword)
+	log.Println("Email has been sent")
+	// Send Email
+	return dialer.DialAndSend(message)
+}
+
+
+
+
+package helpers
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/gomail.v2"
+)
+
+/*
+**
+
+var (
+
+	fromName  string
+	fromEmail string
+	key       string
+
+)
+
+	func init() {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+		fromName = os.Getenv("FROM_NAME")
+		fromEmail = os.Getenv("FROM_EMAIL")
+		key = os.Getenv("SENDGRID_API_KEY")
+
+}
+
+	func SendMail(toName, toEmail string) (string, error) {
+		from := mail.NewEmail(fromName, fromEmail)
+		to := mail.NewEmail(toName, toEmail)
+		subject := "Sending with Twilio SendGrid is Fun"
+		plainTextContent := "and easy to do anywhere, even with Go"
+		htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+
+		message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+
+		client := sendgrid.NewSendClient(key)
+		response, err := client.Send(message)
+		if err != nil {
+			log.Println("SendGrid error:", err)
+			return "", err
+		}
+
+		fmt.Println("Status:", response.StatusCode)
+		fmt.Println("Body:", response.Body)
+		fmt.Println("Headers:", response.Headers)
+
+		return "Welcome message sent successfully", nil
+	}
+
+*
+*/
+var plainText = "and easy to do anywhere, even with Go"
+var htmlContent = fmt.Sprint("<strong> 🔔 HI WELCOME TO OUR APP</strong>")
+
+var subject = "WEKCOME MESSAGE "
+
+func SendMail(toName, toEmail string) error {
+	fromEmail := "parshwanathparamagond1234@gmail.com"
+	//fromPassword := "fbfy zhlt csqr djay"
+	fromPassword := os.Getenv("KEY")
+
+	smtpHost := "smtp.gmail.com"
+	smtpPort := 587
+	message := gomail.NewMessage()
+	message.SetHeader("From", fromEmail)
+	message.SetHeader("To", toEmail)
+	message.SetHeader("Subject", subject)
+	message.SetBody("text/plain", plainText)
+	message.AddAlternative("text/html", htmlContent)
+
+	dialer := gomail.NewDialer(smtpHost, smtpPort, fromEmail, fromPassword)
+	log.Println("Email has been sent")
+	// Send Email
+	return dialer.DialAndSend(message)
+}
+
+
+
+
