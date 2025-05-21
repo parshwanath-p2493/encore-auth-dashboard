81e6972b93b69b246ec5ae6ca5414c40e2efc1d0
Parshwanath Paramagond <parshwanathparamagond1234@gmail.com>
2025-05-21 19:02:28 +0530

    commits

--- helpers/email.go
+++ helpers/email.go
@@ -49,3 +49,30 @@ func SendMail(toName, toEmail string) error {
 	log.Println("Welcome message sent successfully")
 	return nil
 }
+
+/**
+var plainText = "and easy to do anywhere, even with Go"
+var htmlContent = fmt.Sprint("<strong> 🔔 HI WELCOME TO OUR APP</strong>")
+
+var subject = "WEKCOME MESSAGE "
+
+func SendMail(toName, toEmail string) error {
+	fromEmail := "parshwanathparamagond1234@gmail.com"
+	//fromPassword := "fbfy zhlt csqr djay"
+	fromPassword := os.Getenv("KEY")
+
+	smtpHost := "smtp.gmail.com"
+	smtpPort := 587
+	message := gomail.NewMessage()
+	message.SetHeader("From", fromEmail)
+	message.SetHeader("To", toEmail)
+	message.SetHeader("Subject", subject)
+	message.SetBody("text/plain", plainText)
+	message.AddAlternative("text/html", htmlContent)
+
+	dialer := gomail.NewDialer(smtpHost, smtpPort, fromEmail, fromPassword)
+	log.Println("Email has been sent")
+	// Send Email
+	return dialer.DialAndSend(message)
+}
+**/
