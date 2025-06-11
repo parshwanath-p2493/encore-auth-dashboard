328f2e55c8338aa3b821147d50e922d88723c91a
Parshwanath Paramagond <parshwanathparamagond1234@gmail.com>
2025-06-11 23:09:03 +0530

    commit

--- otpandforgotpassword/constants.go
+++ otpandforgotpassword/constants.go
@@ -1,11 +1,1 @@
 package otpandforgotpassword
-
-import "time"
-
-const (
-	OtpExp     = time.Minute * 10
-	otpCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-
-	OTPlength    = 5
-	OtpKeyPrefix = "password-reset"
-)
