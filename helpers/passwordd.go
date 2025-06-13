b4ec977ba1a2bc81666b8a58b8b8f094ffbb8826
Parshwanath Paramagond <parshwanathparamagond1234@gmail.com>
2025-06-13 23:16:55 +0530

    commits

--- helpers/passwordd.go
+++ helpers/passwordd.go
@@ -1,20 +1,2 @@
 package helpers
 
-import (
-	"golang.org/x/crypto/bcrypt"
-)
-
-func HashPsw(Password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(Password), 13)
-	if err != nil {
-		return "", err
-	}
-	return string(bytes), nil
-}
-func VerifyPasw(Password string, HashedPassword string) (string, error) {
-	if err := bcrypt.CompareHashAndPassword([]byte(HashedPassword), []byte(Password)); err != nil {
-		return "Password is incorrect ", err
-	}
-
-	return "Password Matched ", nil
-}
