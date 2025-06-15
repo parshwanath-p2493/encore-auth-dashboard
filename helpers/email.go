3902c960c2a66ddcef97e5f57ce55af5a8ac17a3
Parshwanath Paramagond <parshwanathparamagond1234@gmail.com>
2025-06-15 22:34:13 +0530

    files

--- helpers/email.go
+++ helpers/email.go
@@ -26,51 +26,51 @@ func init() {
 
 }
 
-// func SendMail(toName, toEmail string) error {
-// 	from := mail.NewEmail(fromName, fromEmail)
-// 	to := mail.NewEmail(toName, toEmail)
-// 	subject := "Welcome Message"
-// 	plainTextContent := "and easy to do anywhere, even with Go"
-// 	//htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
-// 	var htmlContent = fmt.Sprint("<strong> 🔔 HI WELCOME TO OUR APP</strong>")
-
-// 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-
-// 	client := sendgrid.NewSendClient(key)
-// 	response, err := client.Send(message)
-// 	if err != nil {
-// 		log.Println("SendGrid error:", err)
-// 		return err
-// 	}
-
-// 	fmt.Println("Status:", response.StatusCode)
-// 	fmt.Println("Body:", response.Body)
-// 	fmt.Println("Headers:", response.Headers)
-// 	log.Println("Welcome message sent successfully")
-// 	return nil
-// }
-
-var plainText = "and easy to do anywhere, even with Go"
-var htmlContent = fmt.Sprint("<strong> 🔔 HI WELCOME TO OUR APP</strong>")
-
-var subject = "WEKCOME MESSAGE "
-
 func SendMail(toName, toEmail string) error {
-	fromEmail := "parshwanathparamagond1234@gmail.com"
-	//fromPassword := "fbfy zhlt csqr djay"
-	fromPassword := os.Getenv("KEY")
+	from := mail.NewEmail(fromName, fromEmail)
+	to := mail.NewEmail(toName, toEmail)
+	subject := "Welcome Message"
+	plainTextContent := "and easy to do anywhere, even with Go"
+	//htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	var htmlContent = fmt.Sprint("<strong> 🔔 HI WELCOME TO OUR APP</strong>")
 
-	smtpHost := "smtp.gmail.com"
-	smtpPort := 587
-	message := gomail.NewMessage()
-	message.SetHeader("From", fromEmail)
-	message.SetHeader("To", toEmail)
-	message.SetHeader("Subject", subject)
-	message.SetBody("text/plain", plainText)
-	message.AddAlternative("text/html", htmlContent)
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
 
-	dialer := gomail.NewDialer(smtpHost, smtpPort, fromEmail, fromPassword)
-	log.Println("Email has been sent")
-	// Send Email
-	return dialer.DialAndSend(message)
+	client := sendgrid.NewSendClient(key)
+	response, err := client.Send(message)
+	if err != nil {
+		log.Println("SendGrid error:", err)
+		return err
+	}
+
+	fmt.Println("Status:", response.StatusCode)
+	fmt.Println("Body:", response.Body)
+	fmt.Println("Headers:", response.Headers)
+	log.Println("Welcome message sent successfully")
+	return nil
 }
+
+// var plainText = "and easy to do anywhere, even with Go"
+// var htmlContent = fmt.Sprint("<strong> 🔔 HI WELCOME TO OUR APP</strong>")
+
+// var subject = "WEKCOME MESSAGE "
+
+// func SendMail(toName, toEmail string) error {
+// 	fromEmail := "parshwanathparamagond1234@gmail.com"
+// 	//fromPassword := "fbfy zhlt csqr djay"
+// 	fromPassword := os.Getenv("KEY")
+
+// 	smtpHost := "smtp.gmail.com"
+// 	smtpPort := 587
+// 	message := gomail.NewMessage()
+// 	message.SetHeader("From", fromEmail)
+// 	message.SetHeader("To", toEmail)
+// 	message.SetHeader("Subject", subject)
+// 	message.SetBody("text/plain", plainText)
+// 	message.AddAlternative("text/html", htmlContent)
+
+// 	dialer := gomail.NewDialer(smtpHost, smtpPort, fromEmail, fromPassword)
+// 	log.Println("Email has been sent")
+// 	// Send Email
+// 	return dialer.DialAndSend(message)
+// }
